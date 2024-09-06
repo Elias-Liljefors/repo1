2599c8bdaaec7154881324742adc565cec27172c
Elias Liljefors <elias.liljefors@proton.me>
2024-09-06 12:38:59 +0300

    Ikäryhmän 18-64 lisääminen
    Lisätty ikäryhmä 18-64

--- ikaohjelma/src/App.java
+++ ikaohjelma/src/App.java
@@ -16,6 +16,13 @@ public class App {
         if (ika >= 16 && ika <= 17) {
             System.out.println("Saat ajaa kevaria"); }
             
+        //Ikäryhmä 18-64
+        if (ika == 18) {
+            System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa"); }
+        else if (ika >= 40 && ika <= 50) {
+            System.out.println("Hyvää parasta keski-ikää"); }
+        else if (ika >= 58 && ika < 65) {
+            System.out.println("Voit mennä varhaiseläkkeelle"); }
 
     }
 }
